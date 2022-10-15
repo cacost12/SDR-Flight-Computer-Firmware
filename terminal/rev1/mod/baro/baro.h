28219eb680b8d8dc08ae9f1c154e258e4f548628
violescents <mnyx0090@gmail.com>
2022-10-15 13:51:00 -0700

    created header file

---
+++ terminal/rev1/mod/baro/baro.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*
+* FILE: 
+* 		baro.h
+*
+* DESCRIPTION: 
+* 		Contains API function for barometric sensor
+*
+*******************************************************************************/
+
+
+// verifies sensor can be accessed
+void baro_get_device_id(
+    void
+);
+
+
+//gets pressure data from sensor
+void baro_get_pressure(
+    void
+);
+
+// gets temp data from sensor
+void baro_get_temp(
+    void
+);
+
+//converts pressure and temp data into altitude --> do research on formula
+void baro_get_altitude(
+    void
+);
